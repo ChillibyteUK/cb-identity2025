0512c2f2e3b1182a84bd272af4d6b95c66b62450
ChillibyteUK <dan@chillibyte.co.uk>
2025-10-28 13:25:39 +0000

    testimonial

--- blocks/cb-testimonial/cb-testimonial.php
+++ blocks/cb-testimonial/cb-testimonial.php
@@ -19,7 +19,7 @@ $background = get_field( 'style' );
 <section id="<?= esc_attr( $block_id ); ?>" class="cb-testimonial <?= esc_attr( $background ); ?>">
     <div class="id-container p-5" data-aos="fade-up">
             <div class="cb-testimonial__quote">
-                “<?= esc_html( get_field( 'quote' ) ); ?>”
+                “<?= wp_kses_post( get_field( 'quote' ) ); ?>”
             </div>
             <div class="cb-testimonial__author">
                 <?= esc_html( get_field( 'author' ) ); ?>
