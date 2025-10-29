83f5e47a79b9e27db2fc3cb1eed8f0543f8cfe1e
ChillibyteUK <dan@chillibyte.co.uk>
2025-10-29 10:34:22 +0000

    insights

---
+++ blocks/cb-recent-news/cb-recent-news.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * CB Recent News Block Template
+ *
+ * @package  cb-identity2025
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Block ID.
+$block_id  = $block['id'] ?? '';
+$blog_type = get_query_var( 'blog_type', 'insights' );
+
+$background = $blog_type;
+switch ( $background ) {
+	case 'press':
+		$background = 'has-purple-900-background-color';
+		$arrow      = '/img/arrow-p200.svg';
+		break;
+	case 'insights':
+		$background = 'has-primary-black-background-color';
+		$arrow      = '/img/arrow-n600.svg';
+		break;
+	default:
+		$background = 'has-primary-black-background-color';
+		$arrow      = '/img/arrow-n600.svg';
+		break;
+}
+
+?>
+<section id="<?php echo esc_attr( $block_id ); ?>" class="cb-latest-insights cb-recent-news <?= esc_attr( $background ); ?>">
+	<div class="cb-latest-insights__pre-title">
+		<div class="id-container pt-2 pb-1 px-5">
+			RECENT NEWS
+		</div>
+	</div>
+	<div class="id-container">
+		<div class="insight-type-grid grid-type-1 id-container p-5">
+			<div class="row g-5">
+			<?php
+			$args = array(
+                'post_type'      => 'post',
+                'post_status'    => array( 'publish' ),
+                'orderby'        => 'date',
+                'order'          => 'DESC', // Descending order.
+                'posts_per_page' => 3,    // Get all posts.
+				'category_name'  => $blog_type,
+            );
+			$q = new WP_Query( $args );
+
+			$counter = 0;
+			while ( $q->have_posts() ) {
+				$q->the_post();
+				++$counter;
+				switch ( $counter ) {
+					case 1:
+						$col_class = 'col-md-3 insight-type-grid__card-1';
+						break;
+					case 2:
+						$col_class = 'col-md-6 insight-type-grid__card-2';
+						break;
+					case 3:
+						$col_class = 'col-md-3 insight-type-grid__card-3';
+						break;
+					default:
+						$col_class = 'col-md-6';
+						break;
+				}
+
+				?>
+			<div class="<?php echo esc_attr( $col_class ); ?>">			
+				<a href="<?php echo esc_url( get_permalink() ); ?>" class="insight-type-grid__card">
+					<div class="insight-type-grid__image-wrapper">
+						<?= get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'insight-type-grid__image' ) ); ?>
+					</div>
+					<div class="insight-type-grid__content">
+						<div class="insight-type-grid__category">
+							<?php
+							$categories = get_the_category();
+							if ( ! empty( $categories ) ) {
+								echo esc_html( $categories[0]->name );
+							}
+							?>
+						</div>
+						<div class="insight-type-grid__title">
+							<?php the_title(); ?>
+						</div>
+						<div class="insight-type-grid__date d-flex align-items-center gap-2">
+							<?php echo get_the_date( 'j F Y' ); ?> 
+ 							<img src="<?php echo esc_url( get_stylesheet_directory_uri() . $arrow ); ?>" width=14 height=13 alt="" />
+						</div>
+					</div>
+				</a>	
+			</div>
+				<?php
+			}
+			wp_reset_postdata();
+			?>
+		</div>
+	</div>
+</section>
--- single.php
+++ single.php
@@ -15,42 +15,78 @@ if ( ! empty( $categories ) && ! is_wp_error( $categories ) ) {
 	// get space separated list of category slugs.
 	$first_category = $categories[0];
 }
-?>
-<main id="main" class="blog">
-	<div class="container">
-		<div class="post_hero">
-			<?= get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'blog_hero__image' ) ); ?>
-		</div>
-		<div class="pt-5 pb-4">
-			<div class="h2">Insights</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3">
-				<div class="category <?= esc_attr( $first_category->slug ); ?>">// <?= esc_html( $first_category->name ); ?></div>
-			</div>
-			<div class="col-md-9 pb-5">
-				<h1 class="h2"><?= esc_html( get_the_title() ); ?></h1>
-				<?php
-				if ( $first_category && (
-					'podcast' === $first_category->slug ||
-					'video' === $first_category->slug ||
-					'interview' === $first_category->slug
-					) ) {
-					$video = get_field( 'video_link' );
-					if ( $video ) {
-						?>
-				<div class="ratio ratio-16x9 mb-4">
-					<iframe src="<?= esc_attr( $video ); ?>"></iframe>	
-				</div>
-						<?php
-					}
-				}
 
+$post_style = $first_category->slug;
+
+switch ( $post_style ) {
+	case 'press':
+		$post_style = 'post-press';
+		break;
+	case 'insights':
+		$post_style = 'post-insight';
+		break;
+	default:
+		$post_style = 'post-press';
+		break;
+}
+
+?>
+<main id="main" class="single-blog <?= esc_attr( $post_style ); ?>">
+	<div class="id-container pt-5 pb-4">
+		<div class="post-hero-clip-group">
+			<?= get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'post-hero-image' ) ); ?>
+		</div>
+	</div>
+	<div class="category-wrapper">
+		<div class="id-container px-5">
+			<div class="category <?= esc_attr( $first_category->slug ); ?>"><?= esc_html( $first_category->name ); ?></div>
+		</div>
+	</div>
+	<div class="post-title">
+		<div class="id-container px-5">
+			<div class="row">
+				<div class="col-md-9">
+					<h1 class="pt-1"><?= esc_html( get_the_title() ); ?></h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row post-content-row mb-5">
+		<div class="col-md-3"></div>
+			<div class="col-md-9 post-content px-5 ps-md-0 pe-md-5">
+				<?php
 				echo apply_filters( 'the_content', get_the_content() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				?>
+				<div class="post-date pt-5">
+					<?= get_the_date( 'j F Y' ); ?>
+				</div>
 			</div>
 		</div>
     </div>
+
+	<section class="recent-news">
+		<?php
+		set_query_var( 'blog_type',  $first_category->slug );
+		get_template_part( 'blocks/cb-recent-news/cb-recent-news' );
+		?>
+	</section>
+	<?php
+
+	// include cta template.
+	set_query_var( 'cta_background', 114 );
+	set_query_var( 'cta_image', 164 );
+	set_query_var( 'cta_title', 'Experience<br>Changes<br>Everything' );
+	set_query_var( 'cta_content', 'What do you want to change?<br>We want to hear what matters most to you.' );
+	set_query_var(
+		'cta_link',
+		array(
+			'url'   => '/contact/',
+			'title' => 'Start your brief',
+		)
+	);
+	get_template_part( 'blocks/cb-cta/cb-cta' );
+
+	?>
 </main>
 <?php
 get_footer();
