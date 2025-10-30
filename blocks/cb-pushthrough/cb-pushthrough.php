a9dbd8665fb3a3a6433a04d473ed8d0a0353c256
ChillibyteDan <dan@chillibyte.co.uk>
2025-10-30 16:11:25 +0000

    pushthrough changes

--- blocks/cb-pushthrough/cb-pushthrough.php
+++ blocks/cb-pushthrough/cb-pushthrough.php
@@ -14,13 +14,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 $block_id = $block['id'] ?? '';
 
 $l = get_field( 'link' );
+
+$background  = get_field( 'background' );
+$link_colour = $background ? 'has-green-400-color' : '';
+$arrow = $background ? 'arrow-g400.svg' : 'arrow-wh.svg';
+if ( $background ) {
+	?>
+<style>
+.cb-pushthrough {
+	--_bg-url: url('<?= esc_url( wp_get_attachment_image_url( $background, 'full' ) ); ?>');
+}
+</style>
+	<?php
+}
 ?>
-<section id="<?php echo esc_attr( $block_id ); ?>" class="cb-pushthrough">
+<section id="<?php echo esc_attr( $block_id ); ?>" class="cb-pushthrough <?php echo $background ? 'has-bg' : ''; ?>">
+	<?php
+	if ( get_field( 'pretitle' ) ) {
+		?>
 	<div class="cb-pushthrough__pretitle">
 		<div class="id-container px-5">
 			<?= esc_html( get_field( 'pretitle' ) ); ?>
 		</div>
 	</div>
+		<?php
+	}
+	?>
+	<?php
+	if ( $background ) {
+		?>
+	<div class="overlay"></div>
+		<?php
+	}
+	?>
 	<div class="id-container px-5">
 		<div class="row py-4">
 			<div class="col-md-6">
@@ -33,9 +59,9 @@ $l = get_field( 'link' );
 				<?php
 				if ( $l && isset( $l['url'], $l['title'] ) ) {
 					?>
-				<a href="<?= esc_url( $l['url'] ); ?>" class="cb-pushthrough__link">
+				<a href="<?= esc_url( $l['url'] ); ?>" class="cb-pushthrough__link <?= esc_attr( $link_colour ); ?>">
 					<?= esc_html( $l['title'] ); ?>&nbsp;
-					<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/arrow-wh.svg' ); ?>" width=33 height=26 alt="" />
+					<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/' . esc_attr( $arrow ) ); ?>" width=33 height=26 alt="" />
 				</a>
 					<?php
 				}
