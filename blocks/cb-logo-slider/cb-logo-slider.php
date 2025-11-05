61ef0dda5d35a3ef9c96befade5f95cce005faaf
ChillibyteUK <dan@chillibyte.co.uk>
2025-11-05 11:22:40 +0000

    client logo slider

--- blocks/cb-logo-slider/cb-logo-slider.php
+++ blocks/cb-logo-slider/cb-logo-slider.php
@@ -16,33 +16,37 @@ $block_id = $block['id'] ?? '';
 ?>
 <section id="<?php echo esc_attr( $block_id ); ?>" class="cb-logo-slider">
 	<div class="id-container cb-logo-slider__marquee">
-		<div class="cb-logo-slider__track">
-			<?php
-			$logos = get_field( 'logos', 'option' );
-			if ( $logos ) {
-				foreach ( $logos as $logo ) {
-					?>
-					<?= wp_get_attachment_image( $logo, 'full', false, array( 'class' => 'cb-logo-slider__logo img-fluid' ) ); ?>
-					<?php
+		<div class="swiper cb-logo-slider__track">
+			<div class="swiper-wrapper">
+				<?php
+				$logos = get_field( 'logos', 'option' );
+				if ( $logos ) {
+					foreach ( $logos as $logo ) {
+						?>
+						<div class="swiper-slide">
+							<?= wp_get_attachment_image( $logo, 'full', false, array( 'class' => 'cb-logo-slider__logo img-fluid' ) ); ?>
+						</div>
+						<?php
+					}
 				}
-				foreach ( $logos as $logo ) {
-					?>
-					<?= wp_get_attachment_image( $logo, 'full', false, array( 'class' => 'cb-logo-slider__logo img-fluid' ) ); ?>
-					<?php
-				}
-				foreach ( $logos as $logo ) {
-					?>
-					<?= wp_get_attachment_image( $logo, 'full', false, array( 'class' => 'cb-logo-slider__logo img-fluid' ) ); ?>
-					<?php
-				}
-				foreach ( $logos as $logo ) {
-					?>
-					<?= wp_get_attachment_image( $logo, 'full', false, array( 'class' => 'cb-logo-slider__logo img-fluid' ) ); ?>
-					<?php
-				}
-			}
-			?>
+				?>
+			</div>
 		</div>
 	</div>
 </section>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  new Swiper('.cb-logo-slider__track', {
+	slidesPerView: 'auto',
+	spaceBetween: 0,
+	loop: true,
+	speed: 6000,
+	autoplay: {
+	  delay: 0,
+	  disableOnInteraction: false,
+	},
+	allowTouchMove: false,
+  });
+});
+</script>
 <?php
