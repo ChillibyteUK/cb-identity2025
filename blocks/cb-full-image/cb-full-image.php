d5c638ff72d2e2e57c1f1bf3b98c230c39dabc4e
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-20 14:32:08 +0000

    full image gradient

--- blocks/cb-full-image/cb-full-image.php
+++ blocks/cb-full-image/cb-full-image.php
@@ -16,6 +16,13 @@ $block_id = $block['id'] ?? '';
 // Output.
 ?>
 <div id="<?php echo esc_attr( $block_id ); ?>" class="cb-full-image">
+	<?php
+	if ( get_field( 'top_gradient' ) ) {
+		?>
+		<div class="overlay"></div>
+		<?php
+	}
+	?>
 	<div class="id-container">
 		<?= wp_get_attachment_image( get_field( 'image' ), 'full', false, array( 'class' => 'img-fluid', 'alt' => get_post_meta( get_field( 'image' ), '_wp_attachment_image_alt', true ) ) ); ?>
 	</div>
