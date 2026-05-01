811d73e0b1b6a368f357b89b3b96fa1b97ac3bee
ChillibyteUK <dan@chillibyte.co.uk>
2026-05-01 16:08:29 +0100

    latest expo work

---
+++ blocks/cb-latest-insights-expo/cb-latest-insights-expo.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * CB Latest Insights Block Template
+ *
+ * @package  cb-identity2025
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Block ID.
+$block_id = $block['id'] ?? '';
+$posts_per_page = intval( get_field( 'posts_per_page' ) ?: 6 );
+
+if ( ! in_array( $posts_per_page, array( 3, 6 ), true ) ) {
+	$posts_per_page = 6;
+}
+
+?>
+<section id="<?php echo esc_attr( $block_id ); ?>" class="cb-latest-insights">
+	<div class="cb-latest-insights__pre-title">
+		<div class="id-container pt-4 pb-3 px-4 px-md-5">
+			LATEST EXPO WORK
+		</div>
+	</div>
+	<div class="id-container py-5 px-4 px-md-5">
+
+		<div class="row g-5">
+			<?php
+			$q = new WP_Query(
+				array(
+					'post_type'      => 'case_study',
+					'posts_per_page' => $posts_per_page,
+					'orderby'        => 'date',
+					'order'          => 'DESC',
+					'tax_query'      => array(
+						array(
+							'taxonomy' => 'theme',
+							'field'    => 'slug',
+							'terms'    => 'expo',
+						),
+					),
+				)
+			);
+			if ( $q->have_posts() ) {
+				$counter = 0;
+				while ( $q->have_posts() ) {
+					$q->the_post();
+					++$counter;
+					switch ( $counter ) {
+						case 1:
+							$col_class = 'col-md-4 col-lg-3 cb-latest-insights__card-1';
+							break;
+						case 2:
+							$col_class = 'col-md-8 col-lg-6 cb-latest-insights__card-2';
+							break;
+						case 3:
+							$col_class = 'col-md-8 col-lg-3 cb-latest-insights__card-3';
+							break;
+						case 4:
+							$col_class = 'col-md-4 col-lg-6 cb-latest-insights__card-4';
+							break;
+						case 5:
+							$col_class = 'col-md-6 col-lg-3 cb-latest-insights__card-5';
+							break;
+						case 6:
+							$col_class = 'col-md-6 col-lg-3 cb-latest-insights__card-6';
+							break;
+						default:
+							$col_class = 'col-md-6';
+							break;
+					}
+
+					?>
+			<div class="<?php echo esc_attr( $col_class ); ?>">			
+				<a href="<?php echo esc_url( get_permalink() ); ?>" class="cb-latest-insights__card">
+					<div class="cb-latest-insights__image-wrapper">
+						<?php
+						echo get_work_image( get_the_ID(), 'cb-latest-insights__image' );
+						?>
+					</div>
+					<div class="cb-latest-insights__content">
+						<div class="cb-latest-insights__title">
+							<?php the_title(); ?>
+						</div>
+						<div class="cb-latest-insights__excerpt">
+							<?php echo wp_kses_post( wp_trim_words( get_the_excerpt(), 18, '...' ) ); ?>
+						</div>
+					</div>
+				</a>	
+			</div>
+					<?php
+				}
+				wp_reset_postdata();
+			}
+			?>
+		</div>
+	</div>
+</section>
